205f76c67eada1964efbd0667f4a9b02c8eb53b1
Jeff Blagdon <jeffblagdon@gmail.com>
2014-02-12 23:15:56 -0500

    Added .gitignore

---
+++ FlickrBook/Store.h
@@ -0,0 +1,13 @@
+//
+//  Store.h
+//  FlickrBook
+//
+//  Created by Blagdon Jeffrey on 2/12/14.
+//  Copyright (c) 2014 Jeffrey Blagdon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Store : NSObject
+
+@end
---
+++ FlickrBook/Store.m
@@ -0,0 +1,13 @@
+//
+//  Store.m
+//  FlickrBook
+//
+//  Created by Blagdon Jeffrey on 2/12/14.
+//  Copyright (c) 2014 Jeffrey Blagdon. All rights reserved.
+//
+
+#import "Store.h"
+
+@implementation Store
+
+@end
